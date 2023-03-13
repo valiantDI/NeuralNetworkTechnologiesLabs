3f08256fda7ee6f1b603210f1f7aa4d50b35b441
dmitry <dmitry.wachromoff@yandex.ru>
2023-03-13 10:15:15 +0300

    BiLSTM-CRF_v2

--- nn-technologies-lab2/data_preprocessing.py
+++ nn-technologies-lab2/data_preprocessing.py
@@ -4,21 +4,20 @@ import moviepy.editor as mp
 from tqdm import tqdm
 from pytube import YouTube
 
-df = pd.read_csv("youtube.csv")
+annotation = pd.read_csv("youtube.csv")
 
 
-def load_data(id, i, category):
+def load_data(i, id, category):
     selected_video = YouTube('http://www.youtube.com/watch?v=' + id)
     audio = selected_video.streams.filter(progressive=True, file_extension='mp4')[0]
     name = audio.download('data/sources/')
     clip = mp.VideoFileClip(name)
-    clip.audio.write_audiofile(f"data/fold{str(category + 1)}/{str(i)}.mp3")
+    clip.audio.write_audiofile(f"data/fold{category}/{str(i)}.mp3")
 
 
-n_c = {"history": 0, "art_music": 1, "travel": 2, "food": 3}
-
-for i in tqdm(range(len(df))):
-    try:
-        load_data(df["link"][i], i, n_c[df["category"][i]])
-    except:
-        pass
+if __name__ == '__main__':
+    for i in tqdm(range(len(annotation))):
+        try:
+            load_data(i, annotation["link"][i], annotation["category"][i])
+        except:
+            print(f"Can't load {annotation['link'][i]} file.")
