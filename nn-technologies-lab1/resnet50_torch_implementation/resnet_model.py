8c3df3ff620b8201b5d4e079f4d21c3cc54000db
dmitry <dmitry.wachromoff@yandex.ru>
2023-03-13 18:33:56 +0300

    BiLSTM-CRF_v3

--- nn-technologies-lab1/resnet50_torch_implementation/resnet_model.py
+++ nn-technologies-lab1/resnet50_torch_implementation/resnet_model.py
@@ -40,36 +40,6 @@ class Bottleneck(nn.Module):
         return x
 
 
-class Block(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_channels, out_channels, i_downsample=None, stride=1):
-        super(Block, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=stride, bias=False)
-        self.batch_norm1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, stride=stride, bias=False)
-        self.batch_norm2 = nn.BatchNorm2d(out_channels)
-
-        self.i_downsample = i_downsample
-        self.stride = stride
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        identity = x.clone()
-
-        x = self.relu(self.batch_norm2(self.conv1(x)))
-        x = self.batch_norm2(self.conv2(x))
-
-        if self.i_downsample is not None:
-            identity = self.i_downsample(identity)
-        print(x.shape)
-        print(identity.shape)
-        x += identity
-        x = self.relu(x)
-        return x
-
-
 class ResNet(nn.Module):
     def __init__(self, ResBlock, layer_list, num_classes, num_channels=3):
         super(ResNet, self).__init__()
@@ -124,3 +94,5 @@ class ResNet(nn.Module):
 
 def ResNet50(num_classes, channels=3):
     return ResNet(Bottleneck, [3, 4, 6, 3], num_classes, channels)
+
+print(ResNet50(4))
